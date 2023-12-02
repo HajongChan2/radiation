675b9b8809a4080532fd63ab598506123ecc6942
HajongChan2 <gktjswn92@naver.com>
2023-12-02 22:30:07 +0900

    aa

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 app.get('/login', (req, res) => {
-    const filePath = __dirname + 'src/login.html';
+    const filePath = __dirname + '/src/login.html';
     res.sendFile(filePath);
 });
 const users = {};
