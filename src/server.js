cb8a3ce65b01f306608cda5cc197c3dec30580b2
HajongChan2 <gktjswn92@naver.com>
2023-12-02 21:59:42 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -21,8 +21,8 @@ const connection = mysql.createConnection({
 });
 
 
-app.use(express.static('dist', { index: false, extensions: ['html', 'js'], 'Content-Type': 'application/javascript' }));
 app.use(express.static(path.join(__dirname, 'dist')));
+
 
 
 app.get('/', (req, res) => {
