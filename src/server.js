4b9f1d9a4c8d200a9d1cc8d48ac72e78951caa4c
HajongChan2 <gktjswn92@naver.com>
2023-12-02 21:56:25 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ app.use(express.static('dist', { index: false, extensions: ['html', 'js'], 'Cont
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 app.get('/login', (req, res) => {
     const filePath = __dirname + '/login.html';
