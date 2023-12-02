bb816dc49e835779ee9f99c155f58897db2cd154
HajongChan2 <gktjswn92@naver.com>
2023-12-02 21:47:27 +0900

    asd

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ app.use(express.static('dist', { index: false, extensions: ['html'] }));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 app.get('/login', (req, res) => {
     const filePath = __dirname + '/login.html';
